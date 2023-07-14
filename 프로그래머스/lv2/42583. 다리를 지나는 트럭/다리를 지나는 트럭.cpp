087bdcdc09c0521a491ab16db4c2351cd81a9f9f
jiwoo <89989211+jwo1024@users.noreply.github.com>
2023-07-14 15:25:30 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
@@ -1,24 +1,23 @@
 #include <string>
 #include <vector>
 #include <deque>
-
-#include <iostream>
+#include <queue>
 
 using namespace std;
 
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
     int answer = 0;
-    deque<pair<int, int> > bridge;
+    queue<pair<int, int> > bridge;
     int current_weight = 0;
     
     for (vector<int>::iterator iter = truck_weights.begin(); iter != truck_weights.end() || !bridge.empty();) {
         if (!bridge.empty() && bridge.front().second == answer - bridge_length) {
             current_weight -= bridge.front().first;
-            bridge.pop_front();
+            bridge.pop();
         }
         if (iter != truck_weights.end() && \
             bridge.size() < bridge_length && current_weight + *iter <= weight) {
-            bridge.push_back(pair<int, int>(*iter, answer));
+            bridge.push(pair<int, int>(*iter, answer));
             current_weight += *iter;
             iter++;
         }
