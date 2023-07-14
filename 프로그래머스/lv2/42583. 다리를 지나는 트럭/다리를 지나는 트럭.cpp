ef2a6c710de4d4f2e50df89364d493acf862731e
jiwoo <89989211+jwo1024@users.noreply.github.com>
2023-07-14 15:19:54 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
@@ -13,18 +13,15 @@ int solution(int bridge_length, int weight, vector<int> truck_weights) {
     
     for (vector<int>::iterator iter = truck_weights.begin(); iter != truck_weights.end() || !bridge.empty();) {
         if (!bridge.empty() && bridge.front().second == answer - bridge_length) {
-            //std::cout << "pop " <<  bridge.front().first << std::endl;
             current_weight -= bridge.front().first;
             bridge.pop_front();
         }
         if (iter != truck_weights.end() && \
             bridge.size() < bridge_length && current_weight + *iter <= weight) {
-            //std::cout << "push " <<  *iter << std::endl;
             bridge.push_back(pair<int, int>(*iter, answer));
             current_weight += *iter;
             iter++;
         }
-//        std::cout << answer << std::endl;
         answer++;
     }
     return answer;
