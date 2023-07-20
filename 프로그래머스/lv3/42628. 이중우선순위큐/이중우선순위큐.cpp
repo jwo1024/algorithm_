5e43459f922988fa79b71f74dd0ef5e64cba60c2
jiwoo <89989211+jwo1024@users.noreply.github.com>
2023-07-20 19:44:23 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.03 ms, Memory: 4.01 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+#include <set>
+#include <iostream>
+
+using namespace std;
+vector<int> solution(vector<string> operations) {
+    vector<int> answer;
+
+    set<int> numbers; // 기본 => less 내림차순 정렬
+    
+    for (vector<string>::iterator iter = operations.begin(); 
+         iter != operations.end(); iter++) {
+        if (*iter == "D 1" && !numbers.empty()) { // 최솟값 삭제
+            numbers.erase(--(numbers.end()));
+        }
+        else if (*iter == "D -1" && !numbers.empty()){ // 최대값 삭제
+            numbers.erase(numbers.begin());
+        }
+        else if ((*iter)[0] == 'I') {// 숫자 삽입
+            (*iter).erase(0, 2);
+            int i = stoi(*iter);
+            numbers.insert(i);
+        }
+        else
+            ;
+    }
+    if (numbers.empty()) {
+        answer.push_back(0); //[0, 0]
+        answer.push_back(0); //[0, 0]
+    }
+    else {
+        answer.push_back(*(--(numbers.end())));
+        answer.push_back(*numbers.begin());
+    }
+    return answer;
+}
