444b0b30d206f453b80fc268c88be9de706886d0
jiwoo <89989211+jwo1024@users.noreply.github.com>
2023-07-20 18:37:58 +0900

    [level 2] Title: 더 맵게, Time: 194.08 ms, Memory: 40.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <iostream>
+
+using namespace std;
+typedef priority_queue<int, std::vector<int>, std::greater<int>> my_type_heap;
+
+int pop_heap(my_type_heap &heap){
+    int top = heap.top();
+    heap.pop();
+    return top;
+}
+
+int solution(vector<int> scoville, int K) {
+    int answer = 0;
+    int new_scoville = 0;
+    
+    if (scoville.empty())
+        return -1;
+    my_type_heap scoville_heap(scoville.begin(), scoville.end());
+    while (scoville_heap.top() < K) {
+        if (scoville_heap.size() < 2)
+            return -1;
+        new_scoville = (pop_heap(scoville_heap) + pop_heap(scoville_heap) * 2);
+        //std::cout << new_scoville << std::endl;
+        scoville_heap.push(new_scoville);
+        answer++;
+    }
+    return answer;
+}
